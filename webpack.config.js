f5d5b4c3b3c831abe8dc803b9332a2d1df2b6d46
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2024-01-14 15:32:13 -0600

    config: configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    mode: "development",
+    entry: {
+        index: './src/index.js',
+        header: './src/header.js',
+        footer: './src/footer.js',
+        home: './src/home.js',
+        menu: './src/menu.js',
+        contact: './src/contact.js',
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'The Golden Apple',
+            template: './src/template.html',
+        }),
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(ttf|otf|eot|woff|woff2)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 };
