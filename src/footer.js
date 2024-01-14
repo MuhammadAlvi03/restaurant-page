5b0a0dc20905b5a688db5b19d2d8a99f21665319
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2024-01-14 17:42:08 -0600

    fix: change class declaration to id for css selector

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,5 @@
 export default function footer() {
-    const footerContainer = document.createElement('div'); footerContainer.className = 'footer';
+    const footerContainer = document.createElement('div'); footerContainer.id = 'footer';
     const footerText = document.createElement('span'); footerText.textContent = 'MuhammadAlvi03';
     footerContainer.appendChild(footerText);
     document.body.appendChild(footerContainer);
