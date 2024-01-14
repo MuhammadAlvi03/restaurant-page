4024f6a903569c5505658b7955fb06be8c202fe2
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2024-01-14 17:22:02 -0600

    refactor: remove reference to main html element

--- src/home.js
+++ src/home.js
@@ -2,11 +2,11 @@ import gappleImgSource from './assets/gapple2.png';
 
 export default function home() {
     const content = document.querySelector('#content');
-    const main = document.createElement('main');
+    const home = document.createElement('div'); home.className = 'home';
     const restaurantName = document.createElement('img');  restaurantName.src = gappleImgSource; restaurantName.alt = 'The Golden Apple';
     const mainPara = document.createElement('p'); mainPara.textContent = 'Crafting flavors, one bite at a time';
     const bigMenuButton = document.createElement('button'); bigMenuButton.className = 'menu-button'; bigMenuButton.textContent = 'View Menu';
 
-    main.append(restaurantName, mainPara, bigMenuButton);
-    content.appendChild(main);
+    home.append(restaurantName, mainPara, bigMenuButton);
+    content.appendChild(home);
 }
