dac519da21bf6111fe1ab1b703fa2e319b765d5f
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2024-01-14 17:20:51 -0600

    feat: add functionality to nav buttons

--- src/header.js
+++ src/header.js
@@ -1,3 +1,6 @@
+import contact from "./contact";
+import home from "./home";
+
 export default function header() {
     const content = document.querySelector('#content');
     const headerContainer = document.createElement('div');
@@ -5,14 +8,48 @@ export default function header() {
     const headerNav = document.createElement('nav');
 
     const buttonNames = ['Home', 'Menu', 'Contact']
-    const buttonArr = [];
-    for (let i = 0; i < 3; i++) {
+    const navButtonsArr = [];
+    for (let i = 0; i < buttonNames.length; i++) {
         let navButton = document.createElement('button');
         navButton.className = 'nav-button';
         navButton.textContent = buttonNames[i];
-        buttonArr.push(navButton);
+        navButtonsArr.push(navButton);
         headerNav.appendChild(navButton);
     }
     headerContainer.appendChild(headerNav);
     document.body.insertBefore(headerContainer, content);
+
+    const [homeButton, menuButton, contactButton] = navButtonsArr;
+
+    homeButton.addEventListener('click', () => {
+        while (content.firstChild) {
+            content.removeChild(content.lastChild);
+        }
+        homeButton.className = 'nav-button-active';
+        menuButton.className = 'nav-button';
+        contactButton.className = 'nav-button';
+        home();
+    })
+
+    menuButton.addEventListener('click', () => {
+        while (content.firstChild) {
+            content.removeChild(content.lastChild);
+        }
+        homeButton.className = 'nav-button';
+        menuButton.className = 'nav-button-active';
+        contactButton.className = 'nav-button';
+        // menu() once done with menu module
+    })
+
+    contactButton.addEventListener('click', () => {
+        while (content.firstChild) {
+            content.removeChild(content.lastChild);
+        }
+        homeButton.className = 'nav-button';
+        menuButton.className = 'nav-button';
+        contactButton.className = 'nav-button-active';
+        contact();
+    })
+
+
 }
