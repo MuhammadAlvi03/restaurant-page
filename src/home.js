d9ae40ab7d856099cd6e4acc7b00643b8feecf4b
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2024-01-15 17:18:59 -0600

    feat: add functionality to menu button

--- src/home.js
+++ src/home.js
@@ -1,4 +1,6 @@
 import gappleImgSource from './assets/gapple2.png';
+import menu from './menu';
+import { getNavButtons } from './header';
 
 export default function home() {
     const content = document.querySelector('#content');
@@ -6,6 +8,16 @@ export default function home() {
     const restaurantName = document.createElement('img');  restaurantName.src = gappleImgSource; restaurantName.alt = 'The Golden Apple';
     const mainPara = document.createElement('p'); mainPara.textContent = 'Crafting flavors, one bite at a time';
     const bigMenuButton = document.createElement('button'); bigMenuButton.className = 'menu-button'; bigMenuButton.textContent = 'View Menu';
+    bigMenuButton.addEventListener('click', () => {
+        while (content.firstChild) {
+            content.removeChild(content.lastChild);
+        }
+        menu();
+        const [homeButton, menuButton, contactButton] = getNavButtons();
+        homeButton.className = 'nav-button';
+        menuButton.className = 'nav-button-active';
+        contactButton.className = 'nav-button';
+    })
 
     home.append(restaurantName, mainPara, bigMenuButton);
     content.appendChild(home);
